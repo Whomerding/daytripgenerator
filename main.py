c3c0bf4c6915c3d03634554744818539338cae86
Whomerding <wendy.homerding@gmail.com>
2022-12-13 08:23:07 -0500

    add lists

---
+++ main.py
@@ -0,0 +1,9 @@
+# lists
+
+destination_list = ['art museum', 'park', 'beach', 'mountains', 'history museum']
+
+restaurant_list = ['Italian', 'Mexican', 'German', 'American', 'Asian Fusian', 'Chinese', 'Thai', 'Korean', 'Indian']
+
+transportation_list = ['the bus', 'your car', 'a plane', 'the train', 'your bike']
+
+entertainment_list = ['live theater', 'movie', 'street performer', 'musical', 'mountain climbing']
